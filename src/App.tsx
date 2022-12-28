a2fcf6d702864568a210f9dc5b01897011c6a9df
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-12-27 22:10:21 -0300

    :sparkles: feat: implementing routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { HashRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Portfólio</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
-export default App;
+function WrappedApp() {
+  return (
+    <HashRouter>
+      <App />
+    </HashRouter>
+  );
+}
+
+export default WrappedApp;
