0e3dfe76abba4604aefaa5f0d6441180006b51fd
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-12-27 22:58:58 -0300

    :test_tube: test: 404 not found route test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,9 @@
 import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
-import App from './App';
+import { MemoryRouter } from 'react-router-dom';
+
+import { WrappedApp, App } from './App';
 
 describe('App', () => {
   // it('', () => {
@@ -9,13 +11,27 @@ describe('App', () => {
   //   // ACT
   //   // EXPECT
   // });
-  it('Render Portfolio', () => {
-    // ARRANGE
-    render(<App />);
-    // ACT
-    // EXPECT
-    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
-      'Portfólio'
-    );
+  describe('Render Pages', () => {
+    it('Render Portfolio', () => {
+      // ARRANGE
+      render(<WrappedApp />);
+      // ACT
+      // EXPECT
+      expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
+        'Portfólio'
+      );
+    });
+
+    it('Renders NOT FOUND page if invalid path', () => {
+      render(
+        <MemoryRouter initialEntries={['/dunof']}>
+          <App />
+        </MemoryRouter>
+      );
+
+      expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
+        '404 - Not Found'
+      );
+    });
   });
 });
